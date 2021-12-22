b099dda98802baebf29d485cc38bd932aacec575
Mikko Halmela <mikko.halmela@helsinki.fi>
2021-12-22 13:56:37 +0200

    smol klean

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-//use rand::Rng;
 use postgres::{Error};
 use agillee::objects::*;
-//use agillee::table::*;
 use agillee::database::*;
 use agillee::cli::*;
 
 
 fn main() -> Result<(), Error> {
-    let db = initialize_db()?;
-    let objs = Objects::new(db)?;
-    let mut cli = CLI::new(objs);
-
-    cli.start()?;
+    CLI::new(Objects::new(initialize_db()?)?)
+        .start()?;
 
     Ok(())
 }
